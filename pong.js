9c149ad2a59adf7d4ca456d28861a101a484eeff
saeedamirii <saeedamirii1383@gmail.com>
2025-02-01 20:55:05 +0330

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,37 +1,46 @@
-// این فایل مخصوص سطح آسان است که شامل آیتم طلایی است.
+// این فایل مخصوص سطح سخت است که شامل آیتم قرمز است.
 
 // اینجا کل کد اصلی بازی را قرار بده و فقط این تغییر را اضافه کن:
 
-let goldenItem = {
+let redItem = {
     x: Math.random() * 500 + 50,
     y: Math.random() * 300 + 50,
     width: 20,
     height: 20,
-    color: "gold",
+    color: "red",
     isActive: true
 };
 
-function drawGoldenItem() {
-    if (goldenItem.isActive) {
-        ctx.fillStyle = goldenItem.color;
-        ctx.fillRect(goldenItem.x, goldenItem.y, goldenItem.width, goldenItem.height);
+function drawRedItem() {
+    if (redItem.isActive) {
+        ctx.fillStyle = redItem.color;
+        ctx.fillRect(redItem.x, redItem.y, redItem.width, redItem.height);
     }
 }
 
-function checkGoldenItemCollision() {
-    if (goldenItem.isActive &&
-        ball.x - ball.radius < goldenItem.x + goldenItem.width &&
-        ball.x + ball.radius > goldenItem.x &&
-        ball.y - ball.radius < goldenItem.y + goldenItem.height &&
-        ball.y + ball.radius > goldenItem.y) {
+function checkRedItemCollision() {
+    if (redItem.isActive &&
+        ball.x - ball.radius < redItem.x + redItem.width &&
+        ball.x + ball.radius > redItem.x &&
+        ball.y - ball.radius < redItem.y + redItem.height &&
+        ball.y + ball.radius > redItem.y) {
 
-        user.score++; // یک امتیاز اضافه شود
-        goldenItem.isActive = false;
+        redItem.isActive = false;
+        let originalControl = user.y;
+        
+        let interval = setInterval(() => {
+            user.y += (Math.random() * 20 - 10); // حرکات تصادفی
+        }, 50);
+
+        setTimeout(() => {
+            clearInterval(interval);
+            user.y = originalControl;
+        }, 3000); // کنترل ۳ ثانیه مختل شود
     }
 }
 
 // در داخل حلقه `update()`
-checkGoldenItemCollision();
+checkRedItemCollision();
 
 // در داخل حلقه `render()`
-drawGoldenItem();
+drawRedItem();
